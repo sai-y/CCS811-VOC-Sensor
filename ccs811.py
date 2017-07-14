0ffdecf7a4ca28b018523ad0b90e50dd7bd72434
Sai Yamanoor <yamanoorsai@gmail.com>
2017-07-13 23:25:18 -0400

    Testing write to None

--- ccs811.py
+++ ccs811.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 	byte = my_ccs811.read_byte_data(0x20)
 	print(format(byte, '02x'))
 
-	my_ccs811.write_byte_data(0xF4, None)
+	my_ccs811.read_byte_data(0xF4)
 	byte = my_ccs811.read_byte_data(0x00)
 	print(format(byte, '02x'))
 	
