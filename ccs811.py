060555cac978f380f7ef977bbfc628fdc03eb90b
Sai Yamanoor <yamanoorsai@gmail.com>
2017-07-16 15:52:50 -0400

    Write_byte test

--- ccs811.py
+++ ccs811.py
@@ -16,13 +16,13 @@ class CCS811(object):
 
 	def write_quick(self, address):
 		self.bus.write_quick(self.device_address)
-	
+
 	def write_block_data(self, address, vals):
 		self.bus.write_i2c_block_data(self.device_address, address, vals)
 
 if __name__ == "__main__":
 	my_ccs811 = CCS811()
-	my_ccs811.write_quick(0xFF, [ 0x11, 0xE5, 0x72, 0x8A])
+	my_ccs811.write_block_data(0xFF, [ 0x11, 0xE5, 0x72, 0x8A])
 	time.sleep(1)
 	byte = my_ccs811.read_byte_data(0x20)
 	print(format(byte, '02x'))
