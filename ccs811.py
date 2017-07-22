b0af95207695eec47b6e17d8ef9c175e0ba84952
Sai Yamanoor <yamanoorsai@gmail.com>
2017-07-21 22:18:40 -0400

    Correction to I2C.Message in sw_reset()

--- ccs811.py
+++ ccs811.py
@@ -8,17 +8,17 @@ class CCS811(object):
 		self.device_address = device_address
 		self.bus = I2C("/dev/i2c-1")
 
-	def read_register(self, address):
+	def read_byte_data(self, address):
+		msgs = [I2C.Message([address], read=False)]
+		self.bus.transfer(self.device_address, msgs)
+
 		msgs = [I2C.Message([address], read=True)]
 		self.bus.transfer(self.device_address, msgs)
-		return msgs[0].data[0]
+		
+		print(msgs[0].data)
 
-	def start_command(self):
-		msg = [I2C.Message([0xF4])]
-		self.bus.transfer(self.device_address, msg)
-
-	def sw_reset(self):
-		msgs = [I2C.Message([0xFF]), I2C.Message([0x11, 0xE5, 0x72, 0x8A])]
+	def write_byte(self, address):
+		msgs = [I2C.Message([address], read=False)]
 		self.bus.transfer(self.device_address, msgs)
 
 	def write_quick(self, address):
@@ -29,12 +29,13 @@ class CCS811(object):
 
 if __name__ == "__main__":
 	my_ccs811 = CCS811()
-	#my_ccs811.sw_reset()
-	print(format(my_ccs811.read_register(0x00), '02x'))
+	#my_ccs811.write_block_data(0xFF, [ 0x11, 0xE5, 0x72, 0x8A])
+	time.sleep(1)
+	my_ccs811.read_byte_data(0x20)
 	#print(byte)
 
 	#my_ccs811.write_byte(0xF4)
-	#my_ccs811.read_byte_data(0x00)
+	my_ccs811.read_byte_data(0x00)
 	#my_ccs811.write_byte(0xF4, 0x00)
 	#byte = my_ccs811.read_byte_data(0x00)
 	#print(format(byte, '02x'))
