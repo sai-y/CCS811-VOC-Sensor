7caca8f784f7b131fe1f493a95aee31006ab1b6f
PRAXAIR-USA\USASXY15 <sai_yamanoor@praxair.com>
2017-12-10 12:38:38 -0500

    Corrections

--- ccs811_nova.py
+++ ccs811_nova.py
@@ -2,7 +2,8 @@ from ccs811 import CCS811
 from Hologram.HologramCloud import HologramCloud
 import time
 
-hologram = HologramCloud(dict(), network='cellular' )
+hologram = HologramCloud(dict(), network='cellular')
+
 
 def post_data(data):
     try:
@@ -20,8 +21,6 @@ if __name__ == "__main__":
     my_ccs811 = CCS811()
     my_ccs811.reset()
 
-    blinkt.set_clear_on_exit(value=True)
-    
     print(my_ccs811.read_byte(0x00))
     print(my_ccs811.read_byte(0x20))
 
@@ -39,15 +38,6 @@ if __name__ == "__main__":
             eco2 = str(data[0] << 8 | data[1])
             voc = str(data[2] << 8 | data[3])
             print(eco2, voc)
-            if voc < 16: 
-                blinkt.set_all(0, 255, 0)
-                FadeInOut(0.05)
-            elif voc < 60:
-                blinkt.set_all(255, 255, 0)
-                FadeInOut(0.05)
-            else:
-                blinkt.set_all(255, 0, 0)
-                FadeInOut(0.05)
 
             if (time.time() - measurement_time) > 90:
                 measurement_time = time.time()
